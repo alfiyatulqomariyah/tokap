b946c13a00359be78415404f834f638fa6abc7ac
alfiyatulqomariyah <alfiyatulqomariyah22@gmail.com>
2019-02-26 09:51:49 +0700

    tokapeps25

--- routes/web.php
+++ routes/web.php
@@ -38,13 +38,14 @@ Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
 			Route::post('/setting','UserSettingController@update');
 	});
 
-	/* Kategori */
+		/* Kategori */
 
-
-	Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
+		Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
 		Route::get('/','KategoriController@daftar')->name('admin.kategori');
 		Route::get('/add','KategoriController@add')->name('admin.kategori.add');
 		Route::post('/add','KategoriController@save');
+		Route::get('/edit/{id}','KategoriController@edit')->name('admin.kategori.edit');
+		Route::post('/edit/{id}','KategoriController@update');
 	});
 
 });
