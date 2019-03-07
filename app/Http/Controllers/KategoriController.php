33a52dbdb345ef88b3132050f5b2eeef1cbb2ca8
alfiyatulqomariyah <alfiyatulqomariyah22@gmail.com>
2019-03-07 09:00:42 +0700

    tokapeps26

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -21,7 +21,8 @@ class KategoriController extends Controller
     }
 
     public function save(Request $req)
-    {   \Validator::make($req->all(),[
+    {   
+        \Validator::make($req->all(),[
                 'kategori'=>'required|between:3,100|unique:kategori,nama_kategori',
             ])->validate();
 
@@ -44,6 +45,10 @@ class KategoriController extends Controller
 
     public function update(Request $req)
     {
+        \Validator::make($req->all(),[
+                'kategori'=>'required|between:3,100|unique:kategori,nama_kategori,'.$req->id,
+        ])->validate();
+
         return 'Fungsi Update';
     }
 }
