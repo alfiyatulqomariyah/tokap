2559ff8f55e7cda12d3ea764d51174283408e564
alfiyatulqomariyah <alfiyatulqomariyah22@gmail.com>
2019-07-09 22:30:22 +0700

    tokap eps 32-33

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produk;
 
 class ProdukController extends Controller
 {
     public function daftar(Request $req)
     {
-    	return view('admin.pages.produk.daftar');
+    	/* Menghubungkan Tabel Produk dengan Kategori */
+    	$data = Produk::join('kategori','kategori.id','produk.id_kategori')
+    			->where('nama_produk','like',"%{$req->keyword}%")
+    			->select('produk.*','nama_kategori')
+    			->orderBy('updated_at','desc')
+    			->paginate(10);
+
+    	return view('admin.pages.produk.daftar',['data'=>$data]);
+    }
+
+    /* Fungsi add/Tambah*/
+    public function add()
+    {
+    	return view('admin.pages.produk.add');
+    }
+
+    /* Fungsi Simpan/Save */
+    public function save(Request $req)
+    {
+    	return 'Fungsi Save';
     }
 }
